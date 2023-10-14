99c94c2c8bf0d70cb3c39144936cde9747927e89
Laping Miklos-Jozsef <lapingmikus@gmail.com>
2023-10-14 20:51:07 +0300

    implemented scytale

--- lab1/crypto.py
+++ lab1/crypto.py
@@ -10,6 +10,7 @@ SUNet: <SUNet ID>
 Replace this with a description of the program.
 """
 from utils import vigenere_add, vigenere_subtract, InvalidKeyException
+from math import ceil
 
 # Caesar Cipher
 
@@ -58,85 +59,32 @@ def decrypt_vigenere(ciphertext, keyword):
 
 print(decrypt_vigenere('LXFOPVEFRNHR', 'LEMON'))
 
-# Merkle-Hellman Knapsack Cryptosystem
+def encrypt_scytale(plaintext: str, circumference: int) -> str:
+    if circumference < 1:
+        raise InvalidKeyException
+    return ''.join([plaintext[start_i::circumference] for start_i in range(circumference)])
 
-def generate_private_key(n=8):
-    """Generate a private key for use in the Merkle-Hellman Knapsack Cryptosystem.
+def decrypt_scytale(ciphertext: str, circumference: int) -> str:
+    if circumference < 1:
+        raise InvalidKeyException
+    step = ceil(len(ciphertext) / circumference)
+    extra_char_count = len(ciphertext) % circumference
+    if extra_char_count == 0:
+        return ''.join([ciphertext[start_i::step] for start_i in range(step)])
+    
+    step_len_separation = step * extra_char_count
+    left, right = ciphertext[:step_len_separation], ciphertext[step_len_separation:]
 
-    Following the instructions in the handout, construct the private key components
-    of the MH Cryptosystem. This consistutes 3 tasks:
+    return ''.join([left[start_i::step] + right[start_i::(step - 1)] for start_i in range(step - 1)]) + left[step - 1::step]
 
-    1. Build a superincreasing sequence `w` of length n
-        (Note: you can check if a sequence is superincreasing with `utils.is_superincreasing(seq)`)
-    2. Choose some integer `q` greater than the sum of all elements in `w`
-    3. Discover an integer `r` between 2 and q that is coprime to `q` (you can use utils.coprime)
+asd = encrypt_scytale('0123456789012', 5)
+print(asd)
+print(decrypt_scytale(asd, 5))
 
-    You'll need to use the random module for this function, which has been imported already
-
-    Somehow, you'll have to return all of these values out of this function! Can we do that in Python?!
-
-    @param n bitsize of message to send (default 8)
-    @type n int
-
-    @return 3-tuple `(w, q, r)`, with `w` a n-tuple, and q and r ints.
-    """
-    raise NotImplementedError  # Your implementation here
-
-
-def create_public_key(private_key):
-    """Create a public key corresponding to the given private key.
-
-    To accomplish this, you only need to build and return `beta` as described in the handout.
-
-        beta = (b_1, b_2, ..., b_n) where b_i = r × w_i mod q
-
-    Hint: this can be written in one line using a list comprehension
-
-    @param private_key The private key
-    @type private_key 3-tuple `(w, q, r)`, with `w` a n-tuple, and q and r ints.
-
-    @return n-tuple public key
-    """
-    raise NotImplementedError  # Your implementation here
-
-
-def encrypt_mh(message, public_key):
-    """Encrypt an outgoing message using a public key.
-
-    1. Separate the message into chunks the size of the public key (in our case, fixed at 8)
-    2. For each byte, determine the 8 bits (the `a_i`s) using `utils.byte_to_bits`
-    3. Encrypt the 8 message bits by computing
-         c = sum of a_i * b_i for i = 1 to n
-    4. Return a list of the encrypted ciphertexts for each chunk in the message
-
-    Hint: think about using `zip` at some point
-
-    @param message The message to be encrypted
-    @type message bytes
-    @param public_key The public key of the desired recipient
-    @type public_key n-tuple of ints
-
-    @return list of ints representing encrypted bytes
-    """
-    raise NotImplementedError  # Your implementation here
-
-
-def decrypt_mh(message, private_key):
-    """Decrypt an incoming message using a private key
-
-    1. Extract w, q, and r from the private key
-    2. Compute s, the modular inverse of r mod q, using the
-        Extended Euclidean algorithm (implemented at `utils.modinv(r, q)`)
-    3. For each byte-sized chunk, compute
-         c' = cs (mod q)
-    4. Solve the superincreasing subset sum using c' and w to recover the original byte
-    5. Reconsitite the encrypted bytes to get the original message back
-
-    @param message Encrypted message chunks
-    @type message list of ints
-    @param private_key The private key of the recipient
-    @type private_key 3-tuple of w, q, and r
-
-    @return bytearray or str of decrypted characters
-    """
-    raise NotImplementedError  # Your implementation here
+"""
+0    5    0
+ 1    6
+  2    7
+   3    8
+    4    9
+"""
