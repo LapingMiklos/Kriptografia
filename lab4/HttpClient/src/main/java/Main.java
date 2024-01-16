054d90e367ad89c4e19a4ab5fa1f47d68b5d33a4
Laping Miklos-Jozsef <lapingmikus@gmail.com>
2024-01-16 19:09:13 +0200

    client now working

--- lab4/HttpClient/src/main/java/Main.java
+++ lab4/HttpClient/src/main/java/Main.java
@@ -1,5 +1,50 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.security.cert.X509Certificate;
+import java.util.Arrays;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSocketFactory;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        try {
+            URL url = new URL("https://bnr.ro/Home-Mobile.aspx");
+            HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+            connection.setSSLSocketFactory((SSLSocketFactory) SSLSocketFactory.getDefault());
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuilder sb = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                sb.append(inputLine);
+            }
+
+            X509Certificate certificate = (X509Certificate) Arrays.stream(connection.getServerCertificates())
+                .filter((e) -> e instanceof X509Certificate)
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("No valid X509certificate"));
+
+            System.out.println("Verzioszam: " + certificate.getVersion());
+            System.out.println("Szeriaszam: " + certificate.getSerialNumber());
+            System.out.println("Tanusito hatosag neve: " + certificate.getIssuerDN());
+            System.out.println("Kibocsatas datuma: " + certificate.getNotBefore());
+            System.out.println("Ervenyessegi ido: " + certificate.getNotAfter());
+            System.out.println("Tanusitvany alanyak adatai: " + certificate.getSubjectDN());
+            System.out.println("Nyilvanos kulcs adatai: " + certificate.getPublicKey());
+
+            in.close();
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter("bnr.html"));
+            writer.write(sb.toString());
+            writer.close();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
